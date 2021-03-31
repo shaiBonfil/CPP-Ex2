389cb75644d6d4088b4b3230afa0fed4810c6717
shaiBonfil <shaibonfil500@gmail.com>
2021-03-31 12:29:13 +0300

    need to fix: message.at(i)

--- Board.cpp
+++ Board.cpp
@@ -1,21 +1,85 @@
-#include <iostream>
+#include <stdexcept>
 #include "Board.hpp"
+#include <iostream>
+#include <map>
 using namespace std;
 using namespace ariel;
 
-namespace ariel{
+const char DEFAULT = '_';
 
-    void Board::post(unsigned int row, unsigned int column, Direction d, string message) {
-        cout << "post() function" << endl;
+//make_pair(row, col)  // make_pair(inner, message.at(i))
+
+namespace ariel
+{
+
+    void Board::post(uint row, uint col, Direction d, string message)
+    {
+        for (int i = 0; i < message.length(); i++)
+        {
+            this->max_row = max(this->max_row, row);
+            this->max_col = max(this->max_col, col);
+            this->min_row = min(this->min_row, row);
+            this->min_col = min(this->min_col, col);
+            
+            inner.insert({row, col});
+            board.insert({inner, message.at(i)});
+            if (d == Direction::Vertical)
+            { //if the direction is Vertical than move row +1
+                row++;
+            }
+            else
+            { // if the direction is Horizontal than move col +1 for next char
+                col++;
+            }
+        }
     }
 
-    string Board::read(unsigned int row, unsigned int column, Direction d, unsigned int length) {
-        cout << "read() function" << endl;
-        return "";
-    }
+    string Board::read(uint row, uint col, Direction d, uint len)
+    {
+        char c = DEFAULT;
+        string res;
+        inner.insert({row, col});
 
-    string Board::show() {
-        return "";
+        for (int i = 0; i < len; i++)
+        {
+            if (board.count(inner))
+            {
+                c = board.at(inner);
+                res += c;
+            }
+            else
+            {
+                c = DEFAULT;
+                res += c;
+            }
+            if (d == Direction::Vertical)
+            { //if the direction is Vertical than move row +1
+                row++;
+            }
+            else
+            { // if the direction is Horizontal than move col +1 for next char
+                col++;
+            }
+        }
+        return res;
     }
-    
-};
+    void Board::show()
+    {
+        for (uint i = (this->min_row)-1; i <= (this->max_row); i++)
+        {
+            cout << i << ": ";
+            for (uint j = (this->min_col); j <= (this->max_col + 1); j++)
+            {
+                inner.insert({i, j});
+                if (board.count(inner))
+                {
+                    cout << board.at(inner) << endl;
+                }
+                else
+                {
+                    cout << DEFAULT << endl;
+                }
+            }
+        }
+    }
+}
--- Board.hpp
+++ Board.hpp
@@ -1,14 +1,37 @@
-#include <iostream>
+#include <string>
 #include "Direction.hpp"
+#include <map>
+#include <iostream>
+#include <stdexcept>
+#include <limits.h>
+
 using namespace std;
 
-namespace ariel{
+namespace ariel
+{
 
-    class Board{
-        public:
+    class Board
+    {
 
-            void post(unsigned int row, unsigned int column, Direction d, string message);
-            string read(unsigned int row, unsigned int column, Direction d, unsigned int length);
-            string show();
+    private:
+        //define object holds the raw and the col on the borad
+        map<uint, uint> inner;
+        map<map<uint, uint>, char> board;
+        uint max_row;
+        uint min_row;
+        uint max_col;
+        uint min_col;
+
+    public:
+        Board()
+        { // constructor
+            max_row = max_col = 0;
+            min_row = min_col = UINT_MAX;
+        }
+        ~Board() {} // destructor
+
+        void post(uint row, uint col, Direction d, string message);
+        string read(uint row, uint col, Direction d, uint len);
+        void show();
     };
-};
+}
